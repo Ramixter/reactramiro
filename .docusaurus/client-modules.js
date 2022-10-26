c63ecc5e7e40bc2b74e7e36d41f6c4c92daa43b2
Ramiro Torres <ramixalex@hotmail.com>
2022-10-26 14:41:07 +0200

    Create client-modules.js

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\Users\\Ramiro\\Documents\\GitHub\\master-cyber\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\Users\\Ramiro\\Documents\\GitHub\\master-cyber\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\Users\\Ramiro\\Documents\\GitHub\\master-cyber\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\Users\\Ramiro\\Documents\\GitHub\\master-cyber\\src\\css\\custom.css'),
+];
